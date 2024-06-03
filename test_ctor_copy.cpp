a710fec6c45deeb730a058c1e9ea732333bc95a8
IzayoiTeku <SSBTdrag999@gmail.com>
2024-06-03 01:08:42 -0400

    Create test_ctor_copy.cpp

---
+++ test_ctor_copy.cpp
@@ -0,0 +1,30 @@
+//  Stack class test program
+//
+//  Tests: Copy Constructor 
+//
+
+#include "ASTree.hpp"
+#include <cassert>
+#include <iostream>
+
+//===========================================================================
+int main ()
+{
+    {
+        //------------------------------------------------------
+        // SETUP FIXTURE
+
+        // TEST
+        std::cout << "Done testing the copy constructor." << std::endl;
+
+        // VERIFY
+        
+    }
+
+
+    // ADD ADDITIONAL TESTS AS NECESSARY
+    
+    
+    return 0;
+}
+
