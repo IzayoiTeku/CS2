b1b24884f5a856022224161cc7472616577f58d7
IzayoiTeku <SSBTdrag999@gmail.com>
2024-06-03 01:09:33 -0400

    Create test_swap.cpp

---
+++ test_swap.cpp
@@ -0,0 +1,29 @@
+//  Stack class test program
+//
+//  Tests: Swap 
+//
+
+#include "ASTree.hpp"
+#include <cassert>
+#include <iostream>
+
+//===========================================================================
+int main ()
+{
+    {
+        //------------------------------------------------------
+        // SETUP FIXTURE
+        
+        // TEST
+        std::cout << "Done testing the swap function." << std::endl;
+
+        // VERIFY
+        
+    }
+
+
+    // ADD ADDITIONAL TESTS AS NECESSARY
+    
+    
+    return 0;
+}
