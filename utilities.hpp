38875511689f418476bf4874b99c84c3f9d1c5b6
IzayoiTeku <SSBTdrag999@gmail.com>
2024-06-03 00:11:33 -0400

    Update utilities.hpp

--- utilities.hpp
+++ utilities.hpp
@@ -1,1 +1,15 @@
+#ifndef UTILITIES_HPP
+#define UTILITIES_HPP
 
+#include <iostream>
+#include "stack.hpp"
+#include <fstream>
+#include "../string/M4/string.hpp"
+
+template <typename T>
+String infix_to_postfix(std::istream&, string<T>&);
+String infix_to_prefix(std::istream&, string<T>&);
+String postfixToAl(const String&);
+
+
+#endif
