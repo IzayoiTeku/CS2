8234de176b4c6331651fa20ab65b7c7bce28874d
IzayoiTeku <SSBTdrag999@gmail.com>
2024-06-03 01:08:05 -0400

    Create test_assignment.cpp

---
+++ test_assignment.cpp
@@ -0,0 +1,30 @@
+//  Stack class test program
+//
+//  Tests: Assignment operator 
+//
+
+#include "ASTree.hpp"
+#include <cassert>
+#include <iostream>
+
+//===========================================================================
+int main ()
+{
+    {
+        //------------------------------------------------------
+        // SETUP FIXTURE
+        
+        // TEST
+    	std::cout << "Done testing the assignment operator." << std::endl;
+
+
+        // VERIFY
+        
+    }
+
+
+    // ADD ADDITIONAL TESTS AS NECESSARY
+    
+    
+    return 0;
+}
