5ceae41aa8e85cda653cc28ddad60c826acaecb2
IzayoiTeku <SSBTdrag999@gmail.com>
2024-06-03 01:09:07 -0400

    Create test_dtor.cpp

---
+++ test_dtor.cpp
@@ -0,0 +1,30 @@
+//  Stack class test program
+//
+//  Tests: Destructor 
+//
+
+#include "ASTree.hpp"
+#include <cassert>
+#include <iostream>
+
+//===========================================================================
+int main ()
+{
+    {
+        //------------------------------------------------------
+        // SETUP FIXTURE
+        ASTree blank_tree();
+        
+        // TEST
+        std::cout << "Done testing the Destructor." << std::endl;
+
+        // VERIFY
+        
+    }
+
+
+    // ADD ADDITIONAL TESTS AS NECESSARY
+    
+    
+    return 0;
+}
